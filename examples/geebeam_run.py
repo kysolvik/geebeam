ed860e6a48ce6fc31a92d1d0295f7029928fc366
Kylen Solvik <kysolvik@gmail.com>
2026-02-19 14:44:34 -0500

    Example with relative years for band naming

--- examples/geebeam_run.py
+++ examples/geebeam_run.py
@@ -35,52 +35,105 @@ config = {
   "patch_size": 128,
   "scale": 500,
   "proj": "EPSG:4326",
-  "n_sample": 200,
+  "n_sample": 5000,
   "validation_ratio": 0.2,
 }
 
 ee.Initialize(project='ksolvik-misc')
 
-# Define your images for sampling
-mcd64 = (ee.ImageCollection('MODIS/061/MCD64A1')
-    .select('BurnDate')
-    .filter(ee.Filter.calendarRange(config['target_year'], config['target_year'], 'year'))
-    .min()
-    )
-mb_burned_area = (
-    ee.Image('projects/mapbiomas-public/assets/brazil/fire/collection4_1/mapbiomas_fire_collection41_annual_burned_v1')
-    .reduceResolution('mean', maxPixels=500)
-    )
-mb_deforestation = (
-    ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_deforestation_secondary_vegetation_v2')
-   .reduceResolution('mode', maxPixels=500)
-)
-deforest_bands = ['deforestation_{}'.format(y) for y in range(1985, 2025)]
-mb_deforestation = (
-    ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_deforestation_secondary_vegetation_v2')
-   .reduceResolution('mode', maxPixels=500)
-   .rename(deforest_bands)
-)
-mb_lulc = (
-    ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_integration_v2')
-    .reduceResolution('mean', maxPixels=500)
-    )
-fwi = (
-    ee.ImageCollection('projects/climate-engine-pro/assets/ce-cems-fire-daily-4-1')
-    .filterDate('{}-01-01'.format(config['target_year']),
-                '{}-04-01'.format(config['target_year']))
-    .max()
-)
-terraclim_mcwd = (
-     ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE')
-    .select('def')
-    .filter(ee.Filter.calendarRange(config['target_year']-1,
-                                    config['target_year']-1,
-                                    'year'))
-    .max()
-)
+# Define images for sampling
+# MODIS MCD64
+def prep_mcd64_year(y):
+    mcd64 = (ee.ImageCollection('MODIS/061/MCD64A1')
+             .select('BurnDate')
+             .filter(ee.Filter.calendarRange(y, y, 'year'))
+             .min()
+             )
+    band_names = mcd64.bandNames().getInfo()
+    band_names_new = ['{}_{}'.format(b, y) for b in band_names]
+    mcd64 = mcd64.rename(band_names_new)
+    return mcd64
+mcd64_list = [prep_mcd64_year(y) for y in range(2001, 2026)]
 
-im_list = [mcd64, mb_burned_area, mb_deforestation, terraclim_mcwd, mb_lulc, fwi]
+# MB Land-use/land-cover
+mb_amz_lulc = (
+    ee.Image('projects/mapbiomas-public/assets/amazon/lulc/collection6/mapbiomas_collection60_integration_v1')
+    .lt(10)
+   .reduceResolution('mean', maxPixels=500)
+)
+mb_amz_lulc_list = [mb_amz_lulc]
+
+# MODIS MOD13
+def prep_mod13_year(y):
+    mod13 = (
+        ee.ImageCollection('MODIS/061/MOD13A1')
+        .select(['NDVI', 'EVI'])
+        .filter(ee.Filter.calendarRange(y,
+                                        y,
+                                        'year'))
+        .max()
+    )
+
+    band_names = mod13.bandNames().getInfo()
+    band_names_new = ['{}_{}'.format(b, y) for b in band_names]
+    mod13 = mod13.rename(band_names_new)
+    return mod13
+mod13_list = [prep_mod13_year(y) for y in range(2001, 2026)]
+
+def prep_mod13_begyear(y):
+    mod13 = (
+        ee.ImageCollection('MODIS/061/MOD13A1')
+        .select(['NDVI', 'EVI', 'sur_refl_b01', 'sur_refl_b02',
+                 'sur_refl_b03','sur_refl_b07'])
+        .filter(ee.Filter.date('{}-01-01'.format(y+1),
+                               '{}-04-30'.format(y+1)))
+        .max()
+    )
+
+    band_names = mod13.bandNames().getInfo()
+    band_names_new = ['{}_begyear_{}'.format(b, y) for b in band_names]
+    mod13 = mod13.rename(band_names_new)
+    return mod13
+mod13_begyear_list = [prep_mod13_begyear(y) for y in range(2001, 2026)]
+
+
+# Define your images for sampling
+def prep_mcwd_year(y):
+    mcwd = (
+        ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE')
+        .select('def')
+        .filter(ee.Filter.calendarRange(y,
+                                        y,
+                                        'year'))
+        .max()
+    )
+    mcwd = mcwd.rename('mcwd_{}'.format(y))
+    return mcwd
+mcwd_list = [prep_mcwd_year(y) for y in range(2001, 2026)]
+
+# Embeddings
+ee_proj = ee.ImageCollection('GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL').first().projection()
+def prep_embeddings_year(y):
+    embeddings = (
+                ee.ImageCollection('GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL')
+                .filter(ee.Filter.calendarRange(y,y,
+                                                'year'))
+                .mosaic()
+                # Special step for embeddings: after mosaic, they don't have proj info
+                .setDefaultProjection(ee_proj)
+                .resample('bicubic')
+                )
+    band_names = embeddings.bandNames().getInfo()
+    band_names_new = ['{}_{}'.format(b, y) for b in band_names]
+    embeddings = embeddings.rename(band_names_new)
+    return embeddings
+
+
+embeddings_list = [prep_embeddings_year(2023)]
+
+im_list = (mcd64_list + mb_amz_lulc_list + mod13_list +
+           mod13_begyear_list + mcwd_list)
+print(im_list)
 
 
 if __name__ == '__main__':
