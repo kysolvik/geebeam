de019b94e45a2b3fc078eaa091d790f914db9aa9
Kylen Solvik <kysolvik@gmail.com>
2026-05-13 15:16:26 -0400

    Remove climate indices download from geebeam

--- src/geebeam/__init__.py
+++ src/geebeam/__init__.py
@@ -1,11 +1,10 @@
 """Beam and Earth Engine helpers for running data pipelines"""
 
-from . import climate_indices, sampler
+from . import sampler
 
 from ._pipeline import run_pipeline, sample_and_run_pipeline, grid_and_run_pipeline
 
 __all__ = [
-    "climate_indices",
     "sampler",
     "run_pipeline",
     "sample_and_run_pipeline",
