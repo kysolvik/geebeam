8c1de1d44646b8fac777f9f468982fac4c41a184
Kylen Solvik <kysolvik@gmail.com>
2026-05-18 09:56:31 -0400

    Explicitly import pipeline submodule

--- src/geebeam/__init__.py
+++ src/geebeam/__init__.py
@@ -5,11 +5,13 @@ try:
     __version__ = version("geebeam")
 except PackageNotFoundError:
     __version__ = "unknown"    
-    
+
+from . import pipeline
 from . import sampler
 from .pipeline import run_pipeline, sample_and_run_pipeline, grid_and_run_pipeline
 
 __all__ = [
+    "pipeline"
     "sampler",
     "run_pipeline",
     "sample_and_run_pipeline",
